baeeb35e9bcb3feef35ec25f20847591e949a5be
MiguelMoreiraDev <contatomigueldev@gmail.com>
2026-02-11 22:03:41 -0300

    ✨ Feat: Add basic routes of app

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,23 @@
+import { NotFoundPage } from "@/pages/not-found-page"
+import { Route, Routes } from "react-router-dom"
+import { AuthLayout } from "./layouts/auth-layout"
+import { FeedPage } from "./pages/feed-page"
+import { LoginPage } from "./pages/login-page"
+import { RegisterPage } from "./pages/register-page"
+import { VerificationEmailPage } from "./pages/verification-email-page"
+
 export function App() {
-  return <h1>Petz | Rede social para pets</h1>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<FeedPage />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+        <Route path="verification-email" element={<VerificationEmailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  )
 }
---
+++ src/paths.ts
@@ -0,0 +1,7 @@
+export const ROUTES = {
+  home: "/",
+  register: "/register",
+  login: "/login",
+  verificationEmail: "/verification-email",
+  forgotPassword: "/esqueci-senha"
+}
