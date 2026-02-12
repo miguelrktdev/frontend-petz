f5862adb7cd7f5caff3cd1e6ac9065c93dacc58e
MiguelMoreiraDev <contatomigueldev@gmail.com>
2026-02-11 23:54:56 -0300

    ✨ Feat: Create user register form request

---
+++ src/hooks/use-user.tsx
@@ -0,0 +1,32 @@
+import { api } from "@/services/axios"
+import type { UserRegisterSchemaInfer } from "@/types/user-types"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import type { AxiosError } from "axios"
+import { useNavigate } from "react-router-dom"
+import { toast } from "sonner"
+
+interface UserRegisterResponse {
+  message: string
+}
+
+export function useRegister() {
+  const querClient = useQueryClient()
+  const navigate = useNavigate()
+  return useMutation({
+    mutationFn: async (payload: UserRegisterSchemaInfer): Promise<UserRegisterResponse> => {
+      const { data } = await api.post("users/register", payload)
+      return data
+    },
+    onSuccess: (data) => {
+      console.log(data.message)
+      querClient.invalidateQueries({ queryKey: ["user"] })
+      toast.success(data.message, { position: "top-left", className: "w-fit" })
+      navigate("/verification-email")
+    },
+    onError: (error: AxiosError<{ message: string }>) => {
+      const errorMessage = error.response?.data.message
+      console.error(errorMessage)
+      toast.error(errorMessage, { position: "top-left", className: "w-fit" })
+    },
+  })
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,18 @@ import { createRoot } from "react-dom/client"
 import { App } from "./app.tsx"
 import "./index.css"
 
+import { Toaster } from "@/components/ui/sonner"
+import { QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter as Router } from "react-router-dom"
+import { queryClient } from "./services/query-client.ts"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <App />
+      </QueryClientProvider>
     </Router>
   </StrictMode>,
 )
